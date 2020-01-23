726778fdb1c4516684d679e078194c8976691ee1
jitammy <jitammy@gmail.com>
2020-01-23 16:22:25 +0800

    solve this.state problem

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = {term: 'What do you want to search?'}
+  state = { term: "" };
 
+  //onFormSubmit 是一个独立的方程，没有state, 所以this.state会出现error, 用arrow function 可以解决binding this 的问题
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
 
-  // this are React controled input
+  // these are React controled input
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" value={this.state.term}onChange={(e)=> this.setState({term: e.target.value})} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
